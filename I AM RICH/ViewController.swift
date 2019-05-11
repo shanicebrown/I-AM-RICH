cac87a7746bd535f685dd334868ff4caf2c28e5e
Shanice Brown <s.mariebrown10@gmail.com>
2019-05-11 10:28:12 -0500

    Initial Commit

---
+++ I AM RICH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Shanice Brown on 1/21/19.
+//  Copyright © 2019 Shanice Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
